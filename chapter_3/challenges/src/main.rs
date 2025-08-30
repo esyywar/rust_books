cc57a0f812646884209c72783ca6d9061a00f4de
Rahul Eswar <rahuleswar@gmail.com>
2025-08-30 14:10:46 -0500

    fib functions tested

--- chapter_3/challenges/src/main.rs
+++ chapter_3/challenges/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 macro_rules! f_to_c {
     ($f:expr) => {
         {
@@ -45,7 +47,65 @@ fn test_celcius_farenheit_conversion() {
     }
 }
 
+fn fib_dp(num: u32) -> u32 {
+    match num {
+        0 => 0,
+        1 => 1,
+        _ => { 
+            let (mut a, mut b) = (0 ,1);
+            for _ in 2..num {
+                let c = a + b;
+                a = b;
+                b = c;
+            }
+            b
+        }
+    }
+}
+
+fn fib_recurse(num: u32) -> u32 {
+    match num {
+        0 => 0,
+        1 => 1,
+        _ => fib_recurse(num - 1) + fib_recurse(num - 2),
+    }
+}
+
+fn test_fib_function(fib_fn: fn(u32) -> u32) {
+    println!("testing fib function...");
+
+    struct FibTestCase {
+        fib_num: u32,
+        value: u32,
+    }
+
+    let test_cases: [FibTestCase; 9] = [
+       FibTestCase { fib_num: 0, value: 0 }, 
+       FibTestCase { fib_num: 1, value: 1 },
+       FibTestCase { fib_num: 2, value: 1 }, 
+       FibTestCase { fib_num: 3, value: 2 }, 
+       FibTestCase { fib_num: 4, value: 3 },
+       FibTestCase { fib_num: 5, value: 5 }, 
+       FibTestCase { fib_num: 6, value: 8 }, 
+       FibTestCase { fib_num: 7, value: 13 },
+       FibTestCase { fib_num: 8, value: 21 },
+    ];
+
+    for test in test_cases {
+        let actual = fib_fn(test.fib_num);
+        match actual.cmp(&test.value) {
+            Ordering::Equal => println!("PASSED"),
+            _ => {
+                println!("FAILED");
+                break;
+            }
+        }
+    }
+}
+
 fn main() {
     test_celcius_farenheit_conversion();
+    test_fib_function(fib_dp);
+    test_fib_function(fib_recurse);
 }
 
